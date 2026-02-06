79515edc044bf6837b1c2fcdbfda6ee6cdaf7936
mk1945 <kmunees24@gmail.com>
2026-02-07 00:33:00 +0530

    feat: add HEAD request support for monitoring

--- cloudvault-backend/src/routes/file.routes.js
+++ cloudvault-backend/src/routes/file.routes.js
@@ -10,14 +10,17 @@ const {
 } = require('../controllers/file.controller');
 const { protect } = require('../middleware/auth.middleware');
 
-router.route('/').get(protect, getUserFiles);
+router.route('/').get(protect, getUserFiles).head((req, res) => res.status(200).end());
 router.route('/folder').post(protect, createFolder);
 router.route('/upload-url').post(protect, getUploadUrl);
 // File sharing (Protected to create link)
 router.get('/:id/share', protect, shareFile);
+router.head('/:id/share', (req, res) => res.status(200).end());
+
 router.route('/:id').delete(protect, deleteFile);
 
 // Access Shared Folder (Public)
 router.get('/shared/:token', getSharedFolder);
+router.head('/shared/:token', (req, res) => res.status(200).end());
 
 module.exports = router;
